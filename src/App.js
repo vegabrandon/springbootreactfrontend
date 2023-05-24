53445469f99e9cb56406fd58eac6d7acfe071b18
vegabrandon <vegabrandon1250@gmail.com>
2023-05-24 17:55:24 -0400

    Added basic form with button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
-
+import Form from './Form';
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App overflow-hidden">
+      <h1
+        className='text-3xl font-bold mt-[5vh]'
+      >
+        User Auth with Java Spring MVC and Spring Data
+      </h1>
+      <Form />
     </div>
   );
 }
